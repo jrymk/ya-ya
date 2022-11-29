4815448a73034db2c25a0767753787b6c1b8e152
DuckyPonp <duckypp920921@gmail.com>
2022-11-29 15:40:42 +0800

    test changes

--- src/yaya.cpp
+++ src/yaya.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
+    sf::CircleShape shape(200.f);
     shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
