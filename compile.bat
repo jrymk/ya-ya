5a7ed028c542240e3d9c108ecc064db9db6e3783
jrymk <jerrymonkeyinc@gmail.com>
2022-11-29 15:32:46 +0800

    First window!

---
+++ compile.bat
@@ -0,0 +1,4 @@
+g++ -c src\yaya.cpp -I.\SFML-2.5.1\include -DSFML_STATIC
+g++ yaya.o -o yaya.exe -L.\SFML-2.5.1\lib -lsfml-graphics-s -lsfml-window-s -lsfml-system-s -lopengl32 -lwinmm -lgdi32
+pause
+yaya.exe
