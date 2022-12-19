31f593aaadd503df5d7af4fd39b88b998125432f
jrymk <jerrymklu@gmail.com>
2022-12-20 01:46:51 +0800

    Missed a } during merge :(
    Signed-off-by: jrymk <jerrymklu@gmail.com>

--- src/yaya.cpp
+++ src/yaya.cpp
@@ -72,65 +72,66 @@ int main() {
                     if (event.key.code == sf::Keyboard::F10) {
                         game.controller.loadTestWorld();  // TBD
                         game.initTruckCollisionBoxes(3, -2);
-                    if (event.key.code == sf::Keyboard::F9) {
-                        game.controller.loadToSaveFile();
-                        game.initTruckCollisionBoxes(3, -2);
-                        game.controller.handleAction(GameController::BTN_START_LOAD_GAME);
+                        if (event.key.code == sf::Keyboard::F9) {
+                            game.controller.loadToSaveFile();
+                            game.initTruckCollisionBoxes(3, -2);
+                            game.controller.handleAction(GameController::BTN_START_LOAD_GAME);
+                        }
+                        case sf::Event::KeyReleased:
+                            game.controls.handleKeyPress(event);
+                        game.controls.handleSoundOnAction(event, audio);
+                        break;
+                        case sf::Event::MouseButtonPressed:
+                        case sf::Event::MouseButtonReleased:
+                            game.controls.handleMousePress(event);
+                        game.controls.handleSoundOnAction(event, audio);
+                        break;
+                        case sf::Event::MouseWheelMoved:
+                            game.controls.handleMouseScroll(event);
+                        break;
                     }
-                case sf::Event::KeyReleased:
-                    game.controls.handleKeyPress(event);
-                    game.controls.handleSoundOnAction(event, audio);
-                    break;
-                case sf::Event::MouseButtonPressed:
-                case sf::Event::MouseButtonReleased:
-                    game.controls.handleMousePress(event);
-                    game.controls.handleSoundOnAction(event, audio);
-                    break;
-                case sf::Event::MouseWheelMoved:
-                    game.controls.handleMouseScroll(event);
-                    break;
             }
+
+            window.clear(sf::Color(129, 214, 131));
+            UIRect rectWindow(sf::FloatRect(0, 0, window.getView().getSize().x, window.getView().getSize().y));
+            Camera::setViewport(rectWindow);
+
+            /// render pass 1
+            if (game.controller.gameState != GameController::TITLE_SCREEN)
+                Camera::setCenter(Camera::getCenter() + (game.player->position - Camera::getCenter()) * 0.1); /// TODO: decide underlying pos or pos
+            Graphics::clearQuadsArray();
+
+            game.controller.update();
+
+            game.update();
+
+            game.controls.update();
+            // this should be AFTER game.update() so when the next main loop comes, the controls event are polled and executed, the states are up to date
+            // but BEFORE renderUI() so it gets the newest states
+
+            game.render();
+            Graphics::renderQuads(window, tilemap, Camera::getViewport());
+
+
+            /// render pass 2
+            Graphics::clearQuadsArray();
+            game.ui.renderUI();
+            Graphics::renderQuads(window, tilemap, Camera::getViewport());
+
+            /// render pass 3 (overlay)
+            Graphics::clearQuadsArray();
+            game.ui.renderOverlay();
+            Graphics::renderQuads(window, tilemap, Camera::getViewport());
+
+
+            window.display();
         }
 
-        window.clear(sf::Color(129, 214, 131));
-        UIRect rectWindow(sf::FloatRect(0, 0, window.getView().getSize().x, window.getView().getSize().y));
-        Camera::setViewport(rectWindow);
+        game.controller.saveToSaveFile();
 
-        /// render pass 1
-        if(game.controller.gameState != GameController::TITLE_SCREEN)
-            Camera::setCenter(Camera::getCenter() + (game.player->position - Camera::getCenter()) * 0.1); /// TODO: decide underlying pos or pos
-        Graphics::clearQuadsArray();
-
-        game.controller.update();
-
-        game.update();
-
-        game.controls.update();
-        // this should be AFTER game.update() so when the next main loop comes, the controls event are polled and executed, the states are up to date
-        // but BEFORE renderUI() so it gets the newest states
-
-        game.render();
-        Graphics::renderQuads(window, tilemap, Camera::getViewport());
-
-
-        /// render pass 2
-        Graphics::clearQuadsArray();
-        game.ui.renderUI();
-        Graphics::renderQuads(window, tilemap, Camera::getViewport());
-
-        /// render pass 3 (overlay)
-        Graphics::clearQuadsArray();
-        game.ui.renderOverlay();
-        Graphics::renderQuads(window, tilemap, Camera::getViewport());
-
-
-        window.display();
-    }
-
-    game.controller.saveToSaveFile();
-
-    #ifdef SUSPEND_CMD
+#ifdef SUSPEND_CMD
         getchar();  // for testing with cmd
-    #endif
-    return 0;
+#endif
+        return 0;
+    }
 }
